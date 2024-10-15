03f2ebb394d84b9cd56eb823b8a88e9f4e8c0e60
harutoreon <norabder567@gmail.com>
2024-10-15 20:25:32 +0900

    Add sample_spec.rb

--- ruby/lib/sample.rb
+++ ruby/lib/sample.rb
@@ -7,9 +7,3 @@ class Payment
     @amount + 500
   end
 end
-
-# payment1 = Payment.new(amount: 500)
-# payment2 = Payment.new(amount: 1000)
-
-# p payment1.total_amount  #=> 1000
-# p payment2.total_amount  #=> 1500
